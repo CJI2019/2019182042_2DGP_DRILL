7e769aa250827de756ac8d40b3fe5c6ce44b9d5e
CJIAsusLabtop <3000mn@naver.com>
2022-10-08 18:46:05 +0900

    배경 천천히 올라가게
    배경을 시간이 지남에 따라 내려가게 함.

--- 2DGP Project/Main.py
+++ 2DGP Project/Main.py
@@ -7,11 +7,21 @@ open_canvas(BackGround_WITDH,BackGround_HEIGHT)
 # Player 정보가 담겨있다
 import PlayerObject
 
-BackGround = load_image("BackGround.jpg")
+BackGround = load_image("back_2_2000.png")
+Main_Floor = load_image("Floor\main_floor_1.png")
+
+BackGroundHeight = 0
+
+PlayerObject.y =(BackGround_HEIGHT//20) + (Main_Floor.h//2) + (PlayerObject.Player_Right_Idle.h//2) 
 
 while PlayerObject.play:
     clear_canvas()
-    BackGround.draw(BackGround_WITDH/2,BackGround_HEIGHT/2)
+    BackGround.clip_draw(0,0+(int)(BackGroundHeight),BackGround.w,BackGround.h-(int)(BackGroundHeight),BackGround_WITDH//2,BackGround_HEIGHT//2)
+    Main_Floor.draw(BackGround_WITDH//2,BackGround_HEIGHT//20)
+
+    # 0.1 씩 배경 이미지 내려가게함.
+    BackGroundHeight += 0.1
+
     PlayerObject.Player_Movement()
     PlayerObject.KeyDown_event()
 
