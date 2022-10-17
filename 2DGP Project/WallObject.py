0e5dc7313df8d5342fe94cd5e281d4de7ee4fa67
CJIAsusLabtop <3000mn@naver.com>
2022-10-17 14:52:04 +0900

    3주 플레이어가 지나갈 수 없는 벽
    플레이어가 지나갈 수 없는 벽을 생성하도록 맵 툴에 넣음. (충돌 미구현)

---
+++ 2DGP Project/WallObject.py
@@ -0,0 +1,23 @@
+from pico2d import *
+# import PlayerObject
+
+class WALL:
+    def __init__(self,x,y):
+        self.image = load_image('wall.png')
+        print("생성")
+        self.x = x
+        self.y = y
+        self.x1 , self.y1= x - self.image.w//2 , y + self.image.h//2
+        self.x2 , self.y2= x + self.image.w//2 , y - self.image.h//2
+        # 화면에 그려질지 말지 플레이어와 부딫히면 그려짐.
+        self.status = False
+    def Draw(self):
+        if not self.status:
+            self.image.draw(self.x,self.y)
+    def Crash(self,player,x,y):
+        # x + player.Right_Idle.w//2 와 y + player.Right_Idle.h//2 는 player x1 , y1 
+        # 이 둘이 벽의 사각형 내에 있으면 막음. 총 4개의 점
+        if (x - player.Right_Idle.w//2 > self.x1 and x - player.Right_Idle.w//2 < self.x2 and
+        y + player.Right_Idle.h//2 < self.y1 and y + player.Right_Idle.h//2 > self.y2):
+            x += 4
+            print("충돌")
